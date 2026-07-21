f59db0858a4a09afaf87b30776134fd97c4edb2f
shanmuga1003 <shanmugap089@gmail.com>
2026-07-21 21:45:04 +0530

    Add files via upload

---
+++ 21-07-26_day_5_task/question 1.py
@@ -0,0 +1,17 @@
+print("PS5 Game Station")
+print("Price")
+print("1. 70 rps for 1 person, 1 TV, for 1 hour.")
+print("2. 60 per person for 2 or 3 people, 1 console, for 1 hour.")
+print("3. 50 per person for 4 people, 1 console, for 1 hour.")
+print("------------------------------------------")
+per = int(input("Enter number of person(1 to 4) : "))
+hour = int(input("Enter number of hour : "))
+
+if per == 1:
+    print(70 * hour)
+elif per == 2 or per == 3:
+    print(60 * per * hour)
+elif per == 4:
+    print(50 * per * hour)
+else:
+    print("not more that 4 person can book a single console")
